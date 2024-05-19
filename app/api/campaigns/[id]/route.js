9fe54ef279f2d85c332ecd861cd170606235e3cf
ZuleenKhan <zuleen.khan670@gmail.com>
2024-05-19 20:06:40 +0530

    final-aim1 push!

--- app/api/campaigns/[id]/route.js
+++ app/api/campaigns/[id]/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import connectMongoDB from '../../../libs/mongodb';
-import Campaign from '../../../models/Campaign';
+import connectMongoDB from '../../../../libs/mongodb';
+import Campaign from '../../../../models/Campaign';
 
 export async function GET(request) {
   await connectMongoDB();
